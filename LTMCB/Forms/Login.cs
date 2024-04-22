7a6c3c431a49524e619645540d5b641cb73780fb
TCkien <145519030+TCkien@users.noreply.github.com>
2024-04-22 09:00:42 +0700

    Add files via upload

---
+++ LTMCB/Forms/Login.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace LTMCB.Forms
+{
+    public partial class Login : Form
+    {
+        IFirebaseClient client;
+        public Login()
+        {
+            InitializeComponent();
+            btnMaxsize.Enabled = false;
+            try
+            {
+                IFirebaseConfig config = new FirebaseConfig
+                {
+                    AuthSecret = "2tMZTGQor4g76nxOQQt93sWgDMmaGgcGddStX13k", // Replace with your Firebase Auth Secret
+                    BasePath = "https://chatandload-default-rtdb.firebaseio.com/" // Replace with your Firebase Database URL
+                };
+
+                client = new FireSharp.FirebaseClient(config);
+
+                if (client != null)
+                {
+                    this.CenterToScreen();
+                    
+                    this.WindowState = FormWindowState.Normal;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Connection Fail.");
+            }
+
+        }
+
+        private void bt_login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_name.Text) || string.IsNullOrEmpty(tb_pass.Text))
+            {
+                MessageBox.Show("Please enter both username and password.");
+            }
+            else
+            {
+                //Looping to get the match data using foreach
+                FirebaseResponse response = client.Get("Users/");
+                Dictionary<string, register> result = response.ResultAs<Dictionary<string, register>>();
+
+                foreach (var get in result)
+                {
+                    string userresult = get.Value.username;
+                    string passresult = get.Value.password;
+
+                    if (tb_name.Text == userresult)
+                    {
+
+                        if (tb_pass.Text == passresult)
+                        {
+                            MessageBox.Show("Đăng nhập thành công", "Thông Báo");
+                            this.Hide();
+                            MainMenu menu = new MainMenu(); 
+                            menu.Show();
+                        }
+
+                    }
+                }
+            }
+        }
+        public class register
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMaxsize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnMinisize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void l_register_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            Forms.FormDangKy dki = new FormDangKy();
+            dki.Show();
+        }
+
+        private void l_forgot_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            Forms.FormQuenMatKhau qmk = new FormQuenMatKhau();
+            qmk.Show();
+        }
+    }
+}
